e1f96835f0698067ae69a4419dcc35b38784c3f0
Baljit Singh <baljitsingh692002@gmail.com>
2024-01-21 15:04:47 +0530

    updated cors

--- app.js
+++ app.js
@@ -20,19 +20,19 @@
 //   })
 // );
 
-// app.use(cors());
-app.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:3005', 'https://apnadaily-blog.netlify.app/'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  return next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   const allowedOrigins = ['http://localhost:3005', 'https://apnadaily-blog.netlify.app/'];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//        res.setHeader('Access-Control-Allow-Origin', origin);
+//   }
+//   //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   return next();
+// });
 
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', 'http://localhost:3001/');
