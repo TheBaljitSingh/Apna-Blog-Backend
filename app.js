0ee9489181e8761c40da0426d03a6eb77f313345
thebaljitsingh <thebaljitsinghin@gmail.com>
2024-05-23 14:47:06 +0530

    updated the cors

--- app.js
+++ app.js
@@ -13,15 +13,20 @@
   
 
 
-// app.use(cors({
-//   origin: process.env.FRONTEND_URL,
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
+  const allowedOrigins = [FRONTEND_URL];
+
+  app.use(cors({
+    origin: function(origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }));
 
 
-app.use(cors());
+// app.use(cors());
 
 
   //Route import kiya
